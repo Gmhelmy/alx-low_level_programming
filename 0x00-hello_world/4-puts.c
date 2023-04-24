d6a9c7ecd30d3a8b032696eb6074ff7453f66a39
gehadmhelmy <gmhelmy@mc.edu.eg>
2023-04-24 16:57:05 -0700

    Hello, World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'My first C Code'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
